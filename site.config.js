14e676ff5ec1287f3d1486025703b6fc8bf34b4b
Ren0503 <illuminati0440@gmail.com>
2021-09-26 14:19:03 +0700

    Add site

--- site.config.js
+++ site.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Forum',
     description: 'Where to learn and focus',
+    siteUrl: 'https://magnifier-forum.herokuapp.com',
     googleAnalytic: 'UA-149699646-1',
     author: {
         email: 'illuminati0440@gmail.com',
